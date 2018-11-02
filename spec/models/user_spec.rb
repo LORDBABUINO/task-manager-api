71fc88bd673b1c44b4aefa8b3d45cc0f55af2e4c
LORDBABUINO <jorge.x7@gmail.com>
2018-11-02 05:54:53 +0000

    Add user authentication token

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,5 +7,30 @@ RSpec.describe User, type: :model do
 	it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
 	it { is_expected.to validate_confirmation_of(:password) }
 	it { is_expected.to allow_value('jorge.x7@gmail.com').for(:email) }
+	it { is_expected.to validate_uniqueness_of(:auth_token)}
 
+	describe '#info' do
+		it 'returns email, created_at and a Token' do
+			user.save!
+			allow(Devise).to receive(:friendly_token).and_return('token')
+
+			expect(user.info).to eq("#{user.email} - #{user.created_at} - Token: token")
+		end
+	end
+
+	describe '#generate_authentication_token!' do
+		it 'generates a unique auth token' do
+			allow(Devise).to receive(:friendly_token).and_return('token')
+			user.generate_authentication_token!
+			expect(user.auth_token).to eq('token')
+		end
+		
+		it 'generates another auth token when the current auth token already has been taken' do
+			allow(Devise).to receive(:friendly_token).and_return('token', 'token', 'token2')
+			existing_user = create(:user)
+			user.generate_authentication_token!
+
+			expect(user.auth_token).not_to eq(existing_user.auth_token)
+		end
+	end
 end
